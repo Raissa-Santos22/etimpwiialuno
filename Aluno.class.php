48468f998f2d49d7d59efdaf609778ae1e2f9132
RAISSA AMORIM DOS SANTOS <RAISSA_4428@uirapuru.br>
2025-08-15 15:14:05 -0300

    Fizemos e testamos os metodos cadastrar e consultar

---
+++ Aluno.class.php
@@ -0,0 +1,91 @@
+<?php
+
+class Aluno{
+    private $id;
+    private $rm;
+    private $nome;
+    private $email;
+    private $cpf;
+    private $pdo;
+
+    public function conectar(){
+       $dns = "mysql:dbname=etimpwiialuno;host=localhost";
+       $dbUser = "root";
+       $dbpass = "";
+
+       try {
+        $this->pdo = new PDO($dns, $dbUser, $dbpass);
+        return true;
+       } catch (\Throwable $th) {
+         return false;
+       }
+    }
+
+    public function getId(){
+        return $this-> id;
+    }
+
+    
+    public function getRm(){
+        return $this-> rm;
+    }
+
+    
+    public function getNome(){
+        return $this-> nome;
+    }
+
+    
+    public function getEmail(){
+        return $this-> email;
+    }
+
+    
+    public function getCpf(){
+        return $this-> cpf;
+    }
+
+    public function setRm($rm){
+        $this-> rm = $rm;
+    }
+
+   
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+
+    public function cadastrar($rm, $nome, $email, $cpf){
+        $sql = "INSERT INTO aluno set rm = :r, nome = :n, email = :e, cpf = :c";
+
+        $sql = $this->pdo->prepare($sql);
+
+        $sql->bindValue(":r", $rm);
+        $sql-> bindValue(":n", $nome);
+        $sql-> bindValue(":e", $email);
+        $sql-> bindValue(":c", $cpf);
+
+        return $sql->execute();
+    }
+
+    public function consulta($email){
+        $sql = "SELECT * FROM aluno WHERE email = :e";
+
+        $sql = $this->pdo->prepare($sql);
+
+        $sql-> bindValue(":e", $email);
+
+        $sql-> execute();
+
+        return $sql->rowCount() > 0;
+    }
+}
---
+++ aluno.php
@@ -0,0 +1,16 @@
+<?php
+require 'Aluno.class.php';
+$aluno = new Aluno();
+
+$con = $aluno->conectar();
+
+if( $con ){
+    $al = $aluno->consulta("kiky@gmail.com");
+    if(!$al ){
+        $aluno->cadastrar(4368, "Quiteria", "kiky@gmail.com", "000.111.222-33");
+    }else{
+        echo "<script>alert('Esse aluno já está cadastrado')</script>";
+    }
+} else{
+    echo"<script>alert('Sem conexão com o banco. Tente novamente mais tarde')<script>";
+}
